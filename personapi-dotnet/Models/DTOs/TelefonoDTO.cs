949a6fad9ddeb3b9fb721e8f45fb0aaf70a2c32a
florezandres <andres.f1850@gmail.com>
2025-05-05 21:35:45 -0500

    Correccion ;

--- personapi-dotnet/Models/DTOs/TelefonoDTO.cs
+++ personapi-dotnet/Models/DTOs/TelefonoDTO.cs
@@ -4,6 +4,6 @@
     {
         public string Num { get; set; } = null!;
         public string Oper { get; set; } = null!;
-        public int Duenio { get; set; };
+        public int Duenio { get; set; }
     }
 }
